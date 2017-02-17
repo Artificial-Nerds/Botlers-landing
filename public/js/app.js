83b94662c397ecf129fba91fd2861210632d1063
Cesar Cardenas <cesar.cardam@gmail.com>
2017-02-17 09:34:34 -0600

    scroll down working correctly

--- public/js/app.js
+++ public/js/app.js
@@ -28,7 +28,6 @@ $(document).ready(function(){
               if(e.which == 13) {
                   var msg = $( "#input-text" ).val();
                   sendUserMessage(msg);
-                  onRowAdded();
                   $( "#input-text" ).val("");
               }
           });
@@ -45,13 +44,15 @@ function sendUserMessage(msg){
   var msg_class = ".messageinner-" + msg_number;
   $(".chat-message-list").append("<li class='message-right " + msg_number + "'><div class='messageinner-" + msg_number + "' hidden><span class='message-text'>" + msg + "</span>" + chatTimeString + "</div></li>");
   $(msg_class).fadeIn();
-  onRowAdded();
   msg_number++;
+  onRowAdded();
   $.ajax({
     url: "botResponse/"+chatId+"/"+msg,
     type: "GET",
     success: function(data,textStatus,jqXHR ){
-      botResponse(data.response);
+      if(data.response!="" && data.response!="null"){
+        botResponse(data.response);
+      }
     }
   });
 }
@@ -61,9 +62,11 @@ function botResponse(msg){
   var msg_class = ".messageinner-" + msg_number;
   var spinner = ".sp-" + msg_number;
   $(".chat-message-list").append("<li class='message-left " + msg_number + "'><div class='sp-" + msg_number + "'><span class='spinme-left'><div class='spinner'><div class='bounce1'></div><div class='bounce2'></div><div class='bounce3'></div></div></span></div><div class='messageinner-" + msg_number + "' hidden><span class='message-text'>" + msg + "</span>" + chatTimeString + "</div></li>");
-  $(spinner).delay(600).hide(1);
+  $(spinner).delay(600).hide();
   $(msg_class).delay(600).fadeIn();
+  setTimeout(function(){onRowAdded();}, 600);
   msg_number++;
+  onRowAdded();
 }
 
 function getTimeString(){
