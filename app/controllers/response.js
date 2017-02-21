1f87d642c63a0b148c42d492aa3ca99a94f4a567
Vicente Guerra <vicenteguerrahdez@gmail.com>
2017-02-21 15:17:57 -0600

    DynamoDB

--- app/controllers/response.js
+++ app/controllers/response.js
@@ -3,10 +3,22 @@
  * Module dependencies.
  */
 var request = require('request');
+var ddb = require('dynamodb').ddb({ accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+                                    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY });
 
 exports.message = function (req, res) {
+  var params = {
+    TableName: 'conversations_botlers.io'
+  };
+  console.log(process.env.AWS_ACCESS_KEY_ID);
+  var item = {conversation_id: "Hdwqiduhweh", msg: "Hola como estas blah blah"};
+
+  ddb.listTables({}, function(err, res) {
+    console.log(res);
+  });
+  console.log("------------------- ** ---------------");
   console.log(req.params);
-  request.post({    
+  request.post({
     url:'http://35.163.243.122:3000/api/v1/sendMessage', json: {
       conversationId: req.params.chatId,
       message: req.params.message
